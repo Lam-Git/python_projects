d1de88c5097c3feee59d28d02db3e8ea8536d646
Lam Nguyen <lsnguyen0@gmail.com>
2021-06-23 23:24:09 -0700

    change

--- weather_app.py
+++ weather_app.py
@@ -2,13 +2,13 @@ import requests
 from pprint import pprint
 
 # MUST OBTAIN AN API KEY
-API_Key = "1b8ffa6ce5fc8154f0cb373b301b9a52"
+API_Key = "f7ac1653b910675dd379b66ae7dc7299"
 
-city = input("Enter a city:")
+city = input("Enter a city or place you would like to visit :")
 base_url = (
     "http://api.openweathermap.org/data/2.5/weather?appid=" + API_Key + "&q" + city
 )
 
-weather_data = request.get(base_url).json()
+weather_data = requests.get(base_url).json()
 
 pprint(weather_data)
